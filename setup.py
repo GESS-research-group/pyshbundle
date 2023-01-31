8351ac17c8997ca19ae08453ef0367e0eec6ea2d
Amin Shakya <65601810+mn5hk@users.noreply.github.com>
2023-01-31 17:13:32 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -53,6 +53,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mn5hk/pyshbundle',
-    version='0.0.1.4',
+    version='0.0.1.6',
     zip_safe=False,
 )
