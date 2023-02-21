2bc4b9ac2257effd0727a61ced4bba491817f8a8
Amin Shakya <65601810+mn5hk@users.noreply.github.com>
2023-02-21 09:05:05 +0530

    update setup pyshbundle 0027

--- setup.py
+++ setup.py
@@ -54,6 +54,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/mn5hk/pyshbundle',
-    version='0.0.2.6',
+    version='0.0.2.7',
     zip_safe=False,
 )
